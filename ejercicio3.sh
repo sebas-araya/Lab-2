f1b8fde531851546af07ad32cd15027cebe1bb96
sebas-araya <arayafukssebastian@gmail.com>
2025-09-10 23:21:46 -0600

    Código del tercer ejercicio. Todavía falta corroborar que corra bien.

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -1,1 +1,9 @@
-echo hola
+directorio="/home/sebasaraya/"
+logfile="/home/sebasaraya/monitoreo2.log"
+
+echo "Se va a monitorear el direcotorio $directorio, los cambios se guardan en $logfile"
+
+inotifywait -m -q -e create modify -e delete "$directorio" |
+while read evento; do
+	echo "$(date "+%Y-%m-%d %H::%M:%S")- $evento">> "$logfile"
+done
