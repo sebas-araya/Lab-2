009730e52fc36631859eee3bed8813b349938f14
sebas-araya <arayafukssebastian@gmail.com>
2025-09-10 23:33:50 -0600

    corrección de errores

--- ejercicio3.sh
+++ ejercicio3.sh
@@ -3,7 +3,7 @@ logfile="/home/sebasaraya/monitoreo2.log"
 
 echo "Se va a monitorear el direcotorio $directorio, los cambios se guardan en $logfile"
 
-inotifywait -m -q -e create modify -e delete "$directorio" |
+inotifywait -m -q -e create,modify,delete "$directorio" |
 while read evento; do
-	echo "$(date "+%Y-%m-%d %H::%M:%S")- $evento">> "$logfile"
+	echo "$(date "+%Y-%m-%d %H:%M:%S")- $evento">> "$logfile"
 done
